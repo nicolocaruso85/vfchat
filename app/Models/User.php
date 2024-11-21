a7536fd927e22155cd8f809ba8259029df251384
nicolocaruso85 <nicolaiphone@live.it>
2024-11-22 00:41:43 +0100

    Aggiunto il modulo per registrare il login degli utenti

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Yadahan\AuthenticationLog\AuthenticationLogable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-  use HasFactory, HasRoles, Notifiable;
+  use AuthenticationLogable, HasFactory, HasRoles, Notifiable;
 
   /**
    * The attributes that are mass assignable.
