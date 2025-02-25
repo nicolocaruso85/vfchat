cb1de9a762550520e5e058c5379e45bac54c3cde
nicolocaruso85 <nicolaiphone@live.it>
2025-02-25 02:43:35 +0100

    Aggiunto link per andare al profilo di un Dipendente

--- resources/views/components/azioni-dipendenti-azienda.blade.php
+++ resources/views/components/azioni-dipendenti-azienda.blade.php
@@ -1,4 +1,8 @@
 <div>
+    <a class="btn btn-icon btn-light-primary btn-sm mr-3" title="Visualizza" href="/utente/{{ $attributes['value'] }}">
+        <i class="bx bx-user bx-md"></i>
+    </a>
+
     <button class="btn btn-icon btn-light-danger btn-sm" title="Elimina" wire:click="$dispatch('openModal', { component: 'elimina-dipendente-azienda', arguments: {{ json_encode(['id_azienda' => $attributes['id_azienda'], 'id_dipendente' => $attributes['value']]) }} })">
         <i class="bx bx-trash bx-md"></i>
     </button>
